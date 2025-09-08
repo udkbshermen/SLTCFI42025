100dfcc6572b1cde7307e7343aa8f7bb99c44976
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-09-08 12:36:14 +0000

    task sheet 111

---
+++ tasksheets/helloWorldApp.java
@@ -0,0 +1,11 @@
+package tasksheets;
+
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp { //completed word "class"
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // completed the word "println"
+    }
+}
