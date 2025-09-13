d773871757f1661c887a12f50ed5dd79fc89adbe
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-09-13 13:59:15 +0000

    task sheet 113

--- tasksheets/tasksheet113.java
+++ tasksheets/tasksheet113.java
@@ -2,11 +2,14 @@ package tasksheets;
 
 public class tasksheet113 {
     public static void main(String[] args) {
-        
-        int check_number = 10;
 
+        int check_number = 10; //initialize length
+
+        //used check_number for loop
         for(int i=1; i<=check_number;i++){
+            //initialize message and input given ternary operation
             String message = (i%2==0)? + i + " is even number." : i + " is odd number.";
+            //print message
             System.out.println(message);
         }
 
